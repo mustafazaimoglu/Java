750d25bfd3db42a42c4460015b0a1aa9f075cfc1
Mustafa Zaimoğlu <mkuz138@gmail.com>
2020-10-08 13:18:02 +0300

    08.10.20

--- Problems/90 degrees Matrix Rotating/src/Main.java
+++ Problems/90 degrees Matrix Rotating/src/Main.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author MKZ
- */
 public class Main
 {
+    public static void rotate(int matrix[][])
+    {
+        
+        
+        
+        
+        
+        
+        
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0 ; j < matrix.length; j++)
+            {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+
+//        for(int i[] : matrix)
+//        {
+//            System.out.println(Arrays.toString(i));
+//        }
+       
+    }
     
+    public static void main(String[] args)
+    {
+        int matrix[][] =
+        {
+            {
+                1, 2, 3
+            },
+            {
+                4, 5, 6
+            },
+            {
+                7, 8, 9
+            }
+        };
+        
+        for(int i[] : matrix)
+        {
+            for(int j : i)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        
+        
+        rotate(matrix);
+    }
 }
